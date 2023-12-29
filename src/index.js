eef3bf674446b75a13f2cfb794422ebc651492c7
gaurav <gaurav30012000jha@gmail.com>
2023-12-29 18:04:23 +0530

    setup sequleize cli orm

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { NAME } = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
+const bodyParser = require("body-parser");
+
+
+const setupAndStartServer = async => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+        // console.log(process.env);
+        console.log(NAME);
+
+    })
+}
+
+setupAndStartServer();
