093b48f8bbce8c4d02def9928087d3807ba4ba11
gaurav <gaurav30012000jha@gmail.com>
2024-01-15 20:49:17 +0530

    made changes to server.js or index.js to callroutes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const setupAndStartServer = async => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
-
+    app.use('./api' , ApiRoutes);// any route starting with api goes to v1 for now 
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
         // console.log(process.env);
